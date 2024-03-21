7421a45c9f96c0fa2df9f0316e5fffe09d42cdbd
rabafaran <rabafaran@gmail.com>
2024-03-21 03:37:15 -0700

    first commit

---
+++ assignment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing the famous quote and its author
+let quote = "nothing is impossible because itself I M POSSIBLE";
+let author = "Raba Faran";
+//printing the message
+console.log(`"${quote}" - ${author}`);
---
+++ assignment 3/main.ts
@@ -0,0 +1,9 @@
+//storing the famous quote and its author
+
+let quote:string = "nothing is impossible because itself I M POSSIBLE"
+
+let author:string = "Raba Faran";
+
+//printing the message
+
+console.log(`"${quote}" - ${author}`);
