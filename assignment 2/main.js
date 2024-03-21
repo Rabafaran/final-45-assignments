873e775e3e0bb2f5673bd78412fbf72f7294a1fd
rabafaran <rabafaran@gmail.com>
2024-03-21 03:29:14 -0700

    first commit

---
+++ assignment 2/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//storing a person name in variable
+let personName = "Raba Faran";
+//printing a person name in lower case
+console.log("Lowercase:", personName.toLowerCase());
+//printing a person name in uppercase
+console.log("uppercase:", personName.toUpperCase());
+//printing a person name in titlecase
+console.log("Titlecase:", `personName.toTitleCase()`);
